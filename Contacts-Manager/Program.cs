dc8d271917a44d2158ad7010105f41e456a129cc
abdallah abuhalima <abdallahabuhalima.1.jo@gmail.com>
2024-06-12 16:18:05 +0300

    Add project files.

---
+++ Contacts-Manager/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Contacts_Manager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
